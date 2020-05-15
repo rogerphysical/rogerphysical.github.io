e88ce8c82d7869c56233256bbe4bf038c5aec63c
rogerphysical <55873122+rogerphysical@users.noreply.github.com>
2020-05-15 09:55:49 +0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 function to_page(page1, page2) {
-	$(page1).animate({left: "-100%"}, 400);
-	$(page2).animate({left: "0%"}, 400);
+	$(page1).animate({left: "-100%"}, 600);
+	$(page2).animate({left: "0%"}, 600);
 }
 function to_page_l(page1, page2) {
-	$(page1).animate({left: "100%"}, 400);
-	$(page2).animate({left: "0%"}, 400);
+	$(page1).animate({left: "100%"}, 600);
+	$(page2).animate({left: "0%"}, 600);
 }
