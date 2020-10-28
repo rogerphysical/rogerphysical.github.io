8a90e88818f8116572511ffa3d4f3be85324b026
rogerphysical <55873122+rogerphysical@users.noreply.github.com>
2020-10-28 20:47:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function to_page(page1, page2) {
+	$(page1).animate({left: "-100%"}, 600);
+	$(page2).animate({left: "0%"}, 600);
+}
+function to_page_l(page1, page2) {
+	$(page1).animate({left: "100%"}, 600);
+	$(page2).animate({left: "0%"}, 600);
+}
+function close1() {
+	var judge = confirm("此動作將清空所有本端localStorage\r確定清除嗎?");
+	if (judge === true) {
+		localStorage.clear();
+	}
+	
+}
+function tips(str) {
+	document.getElementById('tips').innerHTML = str;
+}
